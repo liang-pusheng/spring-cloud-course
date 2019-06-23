2367c554b95f1df59289f92241cdc9e9095c8dab
liangpusheng <hy934954360@gmail.com>
2019-06-23 11:35:22 +0800

    learn Ribbon/Feign

--- cloud-consumer/src/main/java/com/cloudconsumer/controller/HelloRemote.java
+++ cloud-consumer/src/main/java/com/cloudconsumer/controller/HelloRemote.java
@@ -6,6 +6,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * fallback: 指定fallback类，在服务熔断的时候返回fallback类中的内容
+ * name: 提供服务的名称，这是用来创建Ribbon Client(Feign整合了Ribbon)的。
+ *
+ * 注意：如果被调用服务的Controller类上有@RequestMapping注解的话，不可直接在Feign接口上使用@RequestMapping
+ *      注解，启动会报错。可直接将@RequestMapping中的路径注解书写在Feign方法的注解中。
  *
  * @author 梁朴生
  */
